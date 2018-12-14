2bea5d62afb8031faf292bb1bd6fe05f631a32db
Bataron Dimka <batarond@gmail.com>
2018-12-14 15:50:03 +0300

    errorTreeCread

--- Сread.c
+++ Сread.c
@@ -19,20 +19,26 @@ struct BinTree
 	struct BinTree *left;
 	struct BinTree *rigt;
 };
+struct BinTree *funcTree(struct BinTree *st,char *w); 
+struct BinTree *func(struct nameT *Z,FILE *P, int kol);
 struct nameT *returnS();
 int kolS(struct key *keytab);
-struct BinTree *func(struct nameT *Z,FILE *P, int kol);
+int funcP(char *wordP, char *w);
+void treeprint(struct BinTree *P);
 
 
 void main()
 {
 	FILE *fp;
 	struct nameT *KeyWord;
+	struct BinTree *ver;
 	int kol;
 	fp = fopenCon("C:\\1.txt","rt");
-	fclose(fp);
 	KeyWord=returnS();
 	kol = kolS(KeyWord);
+	ver = func(KeyWord,fp,kol);
+	treeprint(ver);
+	fclose(fp);
 }
 FILE *fopenCon(char *way,char *format)
 {// Функция открывающая заданный файл в случае ошибки выводит сообщение
@@ -67,9 +73,10 @@ int kolS(struct nameT *keytab)
 }
 struct BinTree *func(struct nameT *Z,FILE *P, int kol)
 { //Читает из файла слова ищет имена. Возвращает указатель на корень бинарного дерева
-	char z[100],*c,c1;
+	char z[100],*c;
 	int i,k,fl;
-	while(fscanf(P,"%s",z)!=EOF)
+	struct BinTree *ver =NULL;
+	while((fscanf(P,"%s",z))!=EOF)
 	{
 		for(i=0;i<kol;i++)
 		{
@@ -78,20 +85,78 @@ struct BinTree *func(struct nameT *Z,FILE *P, int kol)
 				do
 				{
 					fl=0;
-					while(fgetc(P)==" "); // после типа через пробелы всегда следуют имена;
+					fscanf(P,"%s",z); // слудеющее слово после типа всегда имя.
 					k=0;
-					z[k++]=fgetc(P);
-					// записываем имя по буквам
-					while(z[k]!=" " && z[k]!="," && z[k]!=";") z[k++]=fgetc(P); 
-					if(z[k] ==",") fl=1; // признак того что далее идут так-же имена;
-					z[k]="\0";
+					while(z[k]!=',' && z[k]!='\0' && z[k]!=';') k++; 
+					if(z[k] ==',') // признак того что далее идут так-же имена;
+						{	fl=1; 
+							z[k]='\0';
+						}
+					if(z[k]=';') z[k]='\0';
 					c= (char *) malloc(sizeof(char)*(k+1)); // памть для слова
 					strcpy(c,z);
-				//здесь функция добавления слова в дерево
+					//добавление поученого имени в дерево;
+					ver = funcTree(ver,c);
 				}
 				while(fl==1);// если была запятая то повторить
 			}
 		}
 	}
+	return ver;
 }
+struct BinTree *funcTree(struct BinTree *st,char *w) // нельзя передавать указатель на пустую структуру
+{
+	int cound;
+	if(st == NULL)
+	{
+		st = (struct BinTree *) malloc (sizeof(struct BinTree));
+		st->wordP=w;
+		st->left=NULL;
+		st->rigt=NULL;
+		st->col = 0;
+	}
+	else
+	{
+		if((cound=funcP(st->wordP,w))==0) // слова равны
+		{
+			(st->col)++;
+		}
+		else 
+		{
+			if(cound<0) // первая отличная буква первого слова меньше буквы второго
+			{
+				st->rigt=funcTree(st->rigt,w);
+			}
+			else st->left=funcTree(st->left,w); // больше
+		}
+	}
+	return st;
+}
+int funcP(char *wordP, char *w)
+{
+	int i,fl,z;
+	for(i=0,fl=1;i<7;i++)
+	{
+		//Почему тут не сдвигается
+		if(*(wordP+i)!=*(w+i) || (wordP[i]=='\0' || w[i]=='\0'))
+		{
+			fl=0;
+			break;
+		}
+	}
+	if( fl==0 && wordP[i]=='\0' && wordP[i]=='\0') fl=1;
+	if (fl==1) return 0;
+	else return wordP[i]-w[i]; // >0 последняя буква первого слова старше;
+}
+void treeprint(struct BinTree *P)
+{
+	if(P!=NULL)
+	{
+		treeprint(P->left);
+		printf("%4d%s\n",P->col,P->wordP);
+		treeprint(P->rigt);
+	}
+}
+
+
 
